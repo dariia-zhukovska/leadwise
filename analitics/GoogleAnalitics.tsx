9921e6c3387cfc95a791a51b8ee89930d3e55d16
Dariia Zhukovska <dev18@eliteexperience.at>
2024-01-06 21:31:57 +0200

    added analitics

--- analitics/GoogleAnalitics.tsx
+++ analitics/GoogleAnalitics.tsx
@@ -1,10 +1,7 @@
 
-import Head from 'next/head';
 import Script from 'next/script';
 
 const GoogleAnalytics = () => (
-  <>
-    <Head>
     <div className="container">
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-X89THH5F2F" />
       <Script id="google-analytics">
@@ -17,8 +14,6 @@ const GoogleAnalytics = () => (
         `}
       </Script>
       </div>
-    </Head>
-  </>
 );
 
 export default GoogleAnalytics;
