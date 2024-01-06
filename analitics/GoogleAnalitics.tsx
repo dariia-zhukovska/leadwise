81f9a873e56fe1c73b17dd0b28ad70958fbf63e3
Dariia Zhukovska <dev18@eliteexperience.at>
2024-01-06 23:11:57 +0200

    added analitics

--- analitics/GoogleAnalitics.tsx
+++ analitics/GoogleAnalitics.tsx
@@ -3,14 +3,14 @@ import Script from 'next/script';
 
 const GoogleAnalytics = () => (
     <div className="container">
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFJGYGT8L8" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ZLKL9G1T32" />
       <Script id="google-analytics">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
   
-          gtag('config', 'G-ZFJGYGT8L8');
+          gtag('config', 'G-ZLKL9G1T32');
         `}
       </Script>
       </div>
