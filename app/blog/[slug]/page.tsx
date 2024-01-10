3b22fe2451534dd8b34a91ad42059f01473278f8
Dariia Zhukovska <dev18@eliteexperience.at>
2024-01-10 13:26:32 +0200

    added seo next

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import blogPostData from "@app/api/blogPostData.json";
 
 import { usePathname, useRouter } from "next/navigation";
+import { ArticleJsonLd, NextSeo } from "next-seo";
 // import { Metadata } from "next";
 
 // const postTitle = blogPostData.map((item) => item.mainTitle);
@@ -27,6 +28,54 @@ const BlogPost = () => {
         if (pathname === `/blog/${item.name}`) {
           return (
             <>
+              <>
+                <NextSeo
+                  title={item.mainTitle}
+                  description={item.paragraph01}
+                  canonical={`leadwise.agency/blog/${item.name}`}
+                  openGraph={{
+                    type: "article",
+                    article: {
+                      publishedTime: "2023-01-04T23:04:13Z",
+                      modifiedTime: "2023-01-04T23:04:13Z",
+                      authors: [
+                        "https://www.linkedin.com/in/dmytro-ishler-4828b0134/",
+                      ],
+                      tags: [
+                        "leadgeneration",
+                        "marketing",
+                        "buisness",
+                        "leads",
+                      ],
+                    },
+                    url: "www.example.com/next-seo-blog",
+                    images: [
+                      {
+                        url: "https://leadwise.agency/assets/images/blog/mainPost-01.svg",
+                        width: 850,
+                        height: 650,
+                        alt: "Blof main picture",
+                      },
+                    ],
+                    site_name: "LeadWise Blog",
+                  }}
+                />
+                <ArticleJsonLd
+                  type="BlogPosting"
+                  url={`leadwise.agency/blog/${item.name}`}
+                  title={item.mainTitle}
+                  images={[
+                    "https://leadwise.agency/assets/images/blog/mainPost-01.svg",
+                    // "https://example.com/photos/4x3/photo.jpg",
+                    // "https://example.com/photos/16x9/photo.jpg",
+                  ]}
+                  datePublished="2023-01-04T23:04:13Z"
+                  dateModified="2023-01-04T23:04:13Z"
+                  authorName="Dmytro Ishler"
+                  description={item.paragraph01}
+                />
+                {/* <p>Manage SEO in NextJS with Next SEO - Blog</p> */}
+              </>
               <div className={styles.topInfo}>
                 {" "}
                 <div
