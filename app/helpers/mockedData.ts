34c036cf20a19c56188f515bdbb28c9fb7c94af3
Dariia Zhukovska <dev18@eliteexperience.at>
2024-04-26 20:56:53 +0300

    changed pictures

--- app/helpers/mockedData.ts
+++ app/helpers/mockedData.ts
@@ -4,10 +4,12 @@ const yearNow = new Date().getFullYear();
 export const CALENDLY_LINK = `https://calendly.com/dmytroishler/meeting?month=${yearNow}-${monthNow}`
 
 export const partnerIcons = [
-  // "/assets/images/icons/01.svg",
+  "/assets/images/icons/01.svg",
   "/assets/images/icons/02.svg",
   "/assets/images/icons/03.svg",
-  // "/assets/images/icons/04.svg",
+  "/assets/images/icons/04.svg",
+  "/assets/images/icons/05.svg",
+  "/assets/images/icons/06.svg",
 ];
 
 
