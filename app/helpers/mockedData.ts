f22aab110ab623bd93bc276c2561a68635473666
Dariia Zhukovska <dev18@eliteexperience.at>
2025-10-31 14:36:28 +0200

    fixed calendly link

--- app/helpers/mockedData.ts
+++ app/helpers/mockedData.ts
@@ -1,9 +1,9 @@
 const monthNow = new Date().getMonth() + 1;
 const yearNow = new Date().getFullYear();
 
-// export const CALENDLY_LINK = `https://calendly.com/dmytroishler/meeting?month=${yearNow}-${monthNow}`
+export const CALENDLY_LINK = `https://calendly.com/dmytroishler/meeting?month=${yearNow}-${monthNow}`
 // export const CALENDLY_LINK = `https://calendly.com/oksanalaba-leadwise/30min?month=${yearNow}-${monthNow}`
-export const CALENDLY_LINK = `https://calendly.com/dariiazhukovska-leadwise/30min?month=${yearNow}-${monthNow}`
+// export const CALENDLY_LINK = `https://calendly.com/dariiazhukovska-leadwise/30min?month=${yearNow}-${monthNow}`
 
 
 
